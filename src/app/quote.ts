da449eba891ba0b05837ddf56816aa3849c4d4d0
albright.human <sungutialbright@gmail.com>
2022-04-11 19:05:50 +0300

    declare properties of quotes class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+    showDescription: boolean;
+
+  constructor(
+    public id:number,
+    public name: string,
+    public quote: string,
+    public author: string,
+    public completeDate: Date,
+  ){
+    this.showDescription = false;
+  }
+    
+
 }
